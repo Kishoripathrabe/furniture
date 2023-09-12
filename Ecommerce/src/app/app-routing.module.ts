a3f56965958edb74c364763b5e4866087c4bfaee
“Kishoripathrabe” <kishoripathrabe123@gmail.com>
2023-09-12 20:45:27 +0530

    created files

--- Ecommerce/src/app/app-routing.module.ts
+++ Ecommerce/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent,
+},
+{
+    path:'about', component: AboutComponent 
+},
+{
+    path:'product', component: ProductComponent
+},
+{
+    path:'contactus', component: ContactComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
